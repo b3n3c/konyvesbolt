24a11b273a61b3cc10f8d1cb94f47e539d32bbf9
b3n3c <47054214+b3n3c@users.noreply.github.com>
2023-04-07 14:23:28 +0200

    Adatbázis kapcsolat UTF8-ra állítása

--- backend/conn.php
+++ backend/conn.php
@@ -3,7 +3,7 @@ $_username = "system";
 $_password = "oracle";
 $_connection_string = "localhost/XE";
 
-$conn = oci_connect($_username, $_password, $_connection_string);
+$conn = oci_connect($_username, $_password, $_connection_string, 'AL32UTF8');
 if (!$conn) {
     $e = oci_error();
     trigger_error (htmlentities ($e ['message'], ENT_QUOTES), E_USER_ERROR);

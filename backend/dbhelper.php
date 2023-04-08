a950e2c82b0d502ada835c10c966b6a196e6ecde
b3n3c <47054214+b3n3c@users.noreply.github.com>
2023-04-08 13:13:50 +0200

    getBookByISBN bővítése kiadóval

--- backend/dbhelper.php
+++ backend/dbhelper.php
@@ -14,11 +14,29 @@ function getDb(){
 
 function getBookByISBN($isbn){
     $conn = getDb();
-    $konyv = oci_parse ($conn, 'SELECT * FROM KONYV WHERE ISBN = :value');
+    $konyv = oci_parse ($conn, 'SELECT KONYV.*, KIADO.nev AS kiado FROM KONYV, KIADO WHERE konyv.kiado_id = kiado.kiado_id AND KONYV.ISBN = :value');
     oci_bind_by_name($konyv, ':value', $isbn);
     oci_execute($konyv, OCI_DEFAULT);
     $res = oci_fetch_assoc($konyv);
     oci_free_statement($konyv);
     oci_close($conn);
     return $res;
+}
+
+function getSzerzokString($isbn){
+    $conn = getDb();
+    $szerzok = oci_parse($conn, "SELECT * FROM SZERZO WHERE ISBN = :value");
+    oci_bind_by_name($szerzok, ':value', $isbn);
+    oci_execute($szerzok, OCI_DEFAULT);
+    $i = 1;
+    $k_szerzok = "";
+    while (oci_fetch($szerzok)){
+        if ($i != 1)
+            $k_szerzok .= ", ";
+        $k_szerzok .= oci_result($szerzok, 'VEZETEKNEV'). " " . oci_result($szerzok, 'KERESZTNEV');
+        $i++;
+    }
+    oci_free_statement($szerzok);
+    oci_close($conn);
+    return $k_szerzok;
 }

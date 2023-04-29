e6f1f6336349c9bfb5037e6ac98a674621804b1f
b3n3c <47054214+b3n3c@users.noreply.github.com>
2023-04-29 15:02:44 +0200

    UjRendeles tárolt eljárás használata

--- backend/newOffer.php
+++ backend/newOffer.php
@@ -2,40 +2,31 @@
 include("dbhelper.php");
 session_start();
 $conn = getDb();
-
-$queryCount = oci_parse($conn, "SELECT COUNT(*) FROM RENDELES");
-oci_execute($queryCount);
-$row = oci_fetch_array($queryCount, OCI_RETURN_NULLS+OCI_ASSOC);
-$count = $row['COUNT(*)'];
-$rendeles_id = $count+1;
 $now = date("d-M-y");
 $userId = $_SESSION["id"];
 $ar = (int)$_POST['ar'];
 
-$query = oci_parse($conn, "INSERT INTO RENDELES (RENDELES_ID,TELJESITVE,AR,DATUM,FELHASZNALO_ID)
-VALUES ('$rendeles_id', 'N', '$ar', '$now', $userId)");
-$result = oci_execute($query, OCI_DEFAULT);
-if ($result) {
-    oci_commit($conn);
-    foreach (array_keys($_SESSION["cart"]) as $isbn){
-        $resze_query = oci_parse($conn, "INSERT INTO RESZE(rendeles_id, isbn, darab) VALUES (:rendeles, :isbn, :darab)");
-        oci_bind_by_name($resze_query, ":rendeles", $rendeles_id);
-        oci_bind_by_name($resze_query, ":isbn", $isbn);
-        oci_bind_by_name($resze_query, ":darab", $_SESSION["cart"][$isbn]);
+$sql = 'BEGIN UjRendeles(:param1, :param2, :param3); END;';
+$stmt_id = oci_parse($conn, $sql);
+oci_bind_by_name($stmt_id, ':param1', $userId);
+oci_bind_by_name($stmt_id, ':param2', $now);
+oci_bind_by_name($stmt_id, ':param3', $ar);
+oci_execute($stmt_id);
+oci_commit($conn);
+foreach (array_keys($_SESSION["cart"]) as $isbn){
+    $resze_query = oci_parse($conn, "INSERT INTO RESZE(rendeles_id, isbn, darab) VALUES (:rendeles, :isbn, :darab)");
+    oci_bind_by_name($resze_query, ":rendeles", $rendeles_id);
+    oci_bind_by_name($resze_query, ":isbn", $isbn);
+    oci_bind_by_name($resze_query, ":darab", $_SESSION["cart"][$isbn]);
 
-        if(oci_execute($resze_query, OCI_DEFAULT)){
-            oci_commit($conn);
-            oci_free_statement($resze_query);
-        }else{
-            print "Valami hiba történt";
-            exit();
-        }
+    if(oci_execute($resze_query, OCI_DEFAULT)){
+        oci_commit($conn);
+        oci_free_statement($resze_query);
+    }else{
+        print "Valami hiba történt";
+        exit();
     }
-
-    unset($_SESSION["cart"]);
-    header("Location: ../kosar.php?success=$result#success");
-    exit();
-} else{
-    echo "Hiba !";
-    exit();
 }
+unset($_SESSION["cart"]);
+header("Location: ../kosar.php?success=1#success");
+exit();
